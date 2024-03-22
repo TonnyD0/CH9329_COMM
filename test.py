37a2eb59be2135c615ba2a87137cea7dd8561850
Tonny <TonnyD0@users.noreply.github.com>
2024-03-22 12:30:07 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import serial
 import ch9329Comm
-from ch9329Comm import keyboard
-from ch9329Comm import mouse
 
-keyboard = ch9329Comm.keyboard.DataComm('COM3')  # 开启串口，默认115200
+serial.com3 = serial.Serial('COM3', 115200) # 开启串口
+
+keyboard = ch9329Comm.keyboard.DataComm(serial.com3) # 键盘
 
 # # 检查键盘状态
 print('CH9329 version:', keyboard.version())
@@ -17,21 +17,16 @@ keyboard.type('Hello World!') # 输入Hello World!
 keyboard.send_data('Enter') # 回车换行
 keyboard.release()  # 释放按键  
 
-keyboard.close()
 
-serial.ser = serial.Serial('COM4', 115200)  # 开启串口
+mouse = ch9329Comm.mouse.DataComm(serial.com3, 2560, 1440, 1.25) # 鼠标
 
 # （绝对）鼠标移动到屏幕的左上100*100的位置
-
-# dc = mouse.DataComm()
-# dc.send_data_absolute(500,500)
+mouse.send_data_absolute(100,100)
 
 # （相对）鼠标右移100px 下移100px
-# dc2 = mouse.DataComm()
-# dc2.move_to(-100, -100)
+mouse.move_to_basic(100, 100)
 
 # # 校验
-# dc2 = mouse.DataComm()
-# dc2.move_to(-230,-480)
+# mouse.move_to(-230,-480)
 
-serial.ser.close()  # 关闭串口
+serial.com3.close()  # 关闭串口
